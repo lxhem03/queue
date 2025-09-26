d7d7866e669c4ec6408da02d8e935d24cdb4b2ac
Levhcwm21. <alexags1983@gmail.com>
2025-09-26 14:34:53 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,15 +16,14 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
-    )
+    APP_ID = "27394279"
+    API_HASH = "90a9aa4c31afa3750da5fd686c410851"
+    BOT_TOKEN = "7458039455:AAHHD_cc30a_3Uhpm9ZuNLPbSWSdNQ9a9LY"
+    DEV = 1705634892
+    OWNER = "1705634892"
+    FFMPEG = """
+    ffmpeg -i "{}" -preset faster -c:v libx265 -s 1920x1080 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 20 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1 "{}"
+    """
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config(
         "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
