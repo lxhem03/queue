0171416447deb6c2b4f8407d76a4f872c5982e7e
Levhcwm21. <alexags1983@gmail.com>
2025-09-26 21:01:31 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,14 +19,14 @@ try:
     APP_ID = "27394279"
     API_HASH = "90a9aa4c31afa3750da5fd686c410851"
     BOT_TOKEN = "7458039455:AAHHD_cc30a_3Uhpm9ZuNLPbSWSdNQ9a9LY"
-    DEV = 1705634892
-    OWNER = "1705634892"
+    DEV = 7465574522
+    OWNER = "7465574522"
     FFMPEG = """
     ffmpeg -i "{}" -preset faster -c:v libx265 -s 1920x1080 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 20 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1 "{}"
     """
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
+        "THUMBNAIL", default="https://files.catbox.moe/6ntaju.png"
     )
 except Exception as e:
     print("Environment vars Missing")
