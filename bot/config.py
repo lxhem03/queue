a6d537b72472108123d0a8b4001967d3efe3fac3
Levhcwm21. <alexags1983@gmail.com>
2025-12-18 12:09:50 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,13 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = "27394279"
-    API_HASH = "90a9aa4c31afa3750da5fd686c410851"
-    BOT_TOKEN = "7458039455:AAHHD_cc30a_3Uhpm9ZuNLPbSWSdNQ9a9LY"
+    APP_ID = "23340285"
+    API_HASH = "ab18f905cb5f4a75d41bb48d20acfa50"
+    BOT_TOKEN = "7567477886:AAH_uOFdmt77spRT7S93oelmJgLnUKScEYs"
     DEV = 7465574522
     OWNER = "7465574522"
     FFMPEG = """
-    ffmpeg -i "{}" -preset faster -c:v libx265 -s 1920x1080 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 20 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1 "{}"
+    ffmpeg -i "{}" -vf "drawtext=text='For More Animes In Low MB, Check Out @Animes_Guy in Telegram':x='if(gte(t,240),w-(t-240)*60,NAN)':y=10:fontsize=24:fontcolor=white:enable='gte(t,240)':box=0" -c:v libx265 -crf 29 -s 1920x1080 -c:a libopus -b:a 32k -preset superfast -x265-params pools=2:frame-threads=1:wpp=1:pmode=0:pme=0:no-sao=1:aq-mode=1:aq-strength=0.8:psy-rd=1.0:ref=3:bframes=4:keyint=240:min-keyint=24:rc-lookahead=10 -tune animation -pix_fmt yuv420p -map 0 -c:s copy "{}"
     """
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config(
